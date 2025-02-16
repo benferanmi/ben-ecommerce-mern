27053be444d2420be7600bf7cdd71527db40de2c
Benjamin <benferanmiopafunso@gmail.com>
2025-02-16 22:28:07 +0100

    adding cors options

--- server/server.js
+++ server/server.js
@@ -14,9 +14,26 @@ const port = process.env.PORT || 4000
 connectDB();
 connectCloudinary();
 
+//allowing request from frontend origins 
+const corsOptions = {
+    origin: [
+      'https://ben-ecommerce-mern-admin.vercel.app',
+      'https://ben-ecommerce-mern-frontend.vercel.app'
+    ],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true, 
+    optionsSuccessStatus: 200 
+  };
+
 // middlewares 
 app.use(express.json())
 app.use(cors())
+app.use(cors(corsOptions));
+
+// Handle preflight requests
+app.options('*', cors(corsOptions)); 
+
 
 // api endpoints 
 app.use('/api/user', userRouter)
